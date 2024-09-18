2a82455ee8c08780ce3f85d78f6ecffe5205e708
DIOR FALL <Alexkaad>
2024-09-18 09:54:56 +0200

    Initial commit

---
+++ app/src/main/java/com/alexkaad/e_commerce/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.alexkaad.e_commerce
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.alexkaad.e_commerce.ui.theme.EcommerceTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EcommerceTheme {
+                Scaffold(Modifier.fillMaxSize()) { innerPadding ->
+                    Box(Modifier.padding(innerPadding)) {
+                        Row(Modifier.zIndex(1f).background(Color.Red).fillMaxWidth().height(50.dp), horizontalArrangement = Arrangement.Center) {
+                            Text("coucou")
+                            Icon(Icons.Rounded.Add, "")
+                        }
+                        LazyColumn(Modifier.zIndex(0f)) {
+                            items(count = 50) {
+                                Text("coucou $it", Modifier.height(50.dp).background(Color.Blue))
+                            }
+
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
